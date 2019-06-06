1b203bd3afc3ca0f110e5fc250056a2f3318cc8b
kanemih <kanemitsuh@stf.teu.ac.jp>
2019-06-06 23:20:55 +0900

    Add files via upload

---
+++ w_delegator.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+java    -cp ./classes:nclw/:lib/commons-net-3.6.jar:lib/commons-math-2.0.jar:lib/log4j-api-2.11.1.jar:lib/log4j-core-2.11.1.jar:lib/jackson-core-2.9.8.jar:lib/jackson-databind-2.9.8.jar:lib/jackson-annotations-2.9.8.jar org.ncl.workflow.delegator.NCLWDelegator nclw/env_ncl.json nclw/job_ncl.json nclw2.properties
+
