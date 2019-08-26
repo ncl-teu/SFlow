96cb884d9135295294928e400564059a0425af2b
kanemih <kanemitsuh@stf.teu.ac.jp>
2019-08-26 12:27:58 +0900

    Delete w_worker.sh

--- w_worker.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-java    -cp ./classes:nclw/:lib/ganymed-ssh2-build210.jar:lib/commons-net-3.6.jar:lib/commons-math-2.0.jar:lib/log4j-api-2.11.1.jar:lib/log4j-core-2.11.1.jar:lib/jackson-core-2.9.8.jar:lib/jackson-databind-2.9.8.jar:lib/jackson-annotations-2.9.8.jar org.ncl.workflow.main.NCLWMain 1 nclw2.properties
