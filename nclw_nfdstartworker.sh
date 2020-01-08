dd9077319fe1328db07fee92ef67c5640722eaf3
kanemih <kanemitsuh@stf.teu.ac.jp>
2020-01-09 02:11:45 +0900

    Add files via upload

---
+++ nclw_nfdstartworker.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+java    -cp nclw/:lib/ncl-taskschedsim.jar:lib/ncl-sfc.jar:lib/ncl-sfcworkflow.jar:lib/commons-net-3.6.jar:lib/commons-math-2.0.jar:lib/log4j-api-2.11.1.jar:lib/ganymed-ssh2-build210.jar:lib/log4j-core-2.11.1.jar:lib/jackson-core-2.9.8.jar:lib/jackson-databind-2.9.8.jar:lib/jackson-annotations-2.9.8.jar:lib/jsch-0.1.55.jar  org.ncl.workflow.main.WorkerControl  start nclw_hosts ./nclw_nfd.sh
