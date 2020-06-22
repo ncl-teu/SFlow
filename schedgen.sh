51f3a1c633f433c0d8a17cae666e60059e7d3604
kanemih <kanemitsuh@stf.teu.ac.jp>
2020-06-22 13:25:27 +0900

    Add files via upload

---
+++ schedgen.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+java    -cp nclw/:lib/ncl-taskschedsim.jar:lib/ncl-sfc.jar:lib/ncl-sfcworkflow.jar:lib/commons-net-3.6.jar:lib/commons-math-2.0.jar:lib/log4j-api-2.11.1.jar:lib/ganymed-ssh2-build210.jar:lib/log4j-core-2.11.1.jar:lib/jackson-core-2.9.8.jar:lib/jackson-databind-2.9.8.jar:lib/jackson-annotations-2.9.8.jar:lib/jsch-0.1.55.jar:lib/oshi-core-4.4.2.jar:lib/slf4j-api-1.7.30.jar:lib/jna-platform-5.5.0.jar:lib/jna-5.5.0.jar org.ncl.workflow.ccn.sfc.main.SFCSchedJsonGenerator $1 $2 nclw2.properties $3
