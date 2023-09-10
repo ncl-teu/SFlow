65c662eff7287ffd7cc3469ff111e6033ec16632
fkawabata <g21230055c@edu.teu.ac.jp>
2023-09-10 17:28:01 +0900

    Add(setupssh.sh):SSHのレガシーアルゴリズムを有効化

---
+++ setupssh.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+echo 'KexAlgorithms diffie-hellman-group1-sha1,diffie-hellman-group14-sha1,diffie-hellman-group-exchange-sha1,diffie-hellman-group-exchange-sha256,ecdh-sha2-nistp256,ecdh-sha2-nistp384,ecdh-sha2-nistp521,diffie-hellman-group1-sha1,curve25519-sha256@libssh.org' >> /etc/ssh/sshd_config
+systemctl reload sshd.service
+echo 'Enable Legacy Algorithm'
